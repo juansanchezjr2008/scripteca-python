8d95a0707668c31a11dd62e4e1b89bf27feda256
3rick5h <225248070+3rick-sh@users.noreply.github.com>
2025-09-05 18:56:14 -0600

    Create [Condicionales].py

---
+++ Basic Python/[Condicionales].py
@@ -0,0 +1,11 @@
+'''
+En este apartado veremos los condicionales, que son estructuras de control
+que permiten ejecutar diferentes bloques de código según ciertas condiciones.
+'''
+# Ejemplo de condicional simple
+edad = 18
+if edad >= 18:
+    print("Eres mayor de edad")
+# En este codigo se verifica si la variable 'edad' es mayor o igual a 18.
+# Si es verdadero, se imprime "Eres mayor de edad".
+
